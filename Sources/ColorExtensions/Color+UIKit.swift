e2836716c65155b559830098ea578bedffe8bf6f
Sam Deane <sam@elegantchaos.com>
2022-07-19 15:19:23 +0100

    Added some more colors.

--- Sources/ColorExtensions/Color+UIKit.swift
+++ Sources/ColorExtensions/Color+UIKit.swift
@@ -11,14 +11,25 @@ public extension Color {
     // named colours not exposed by SwiftUI
     static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
     static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
-    static let background = Color(UIColor.systemBackground)
-    static let background2 = Color(UIColor.secondarySystemBackground)
+    static let tertiarySystemGroupedBackground = Color(UIColor.tertiarySystemGroupedBackground)
+
+    static let systemFill = Color(UIColor.systemFill)
+    static let secondarySystemFill = Color(UIColor.secondarySystemFill)
+    static let tertiarySystemFill = Color(UIColor.tertiarySystemFill)
+    static let quaternarySystemFill = Color(UIColor.quaternarySystemFill)
+
+    static let systemBackground = Color(UIColor.systemBackground)
+    static let secondarySystemBackground = Color(UIColor.secondarySystemBackground)
+    static let tertiarySystemBackground = Color(UIColor.tertiarySystemBackground)
+    
     static let label = Color(UIColor.label)
     static let secondaryLabel = Color(UIColor.secondaryLabel)
     static let tertiaryLabel = Color(UIColor.tertiaryLabel)
     static let quaternaryLabel = Color(UIColor.quaternaryLabel)
-    static let lightText = Color(UIColor.lightText)
     
+    static let lightText = Color(UIColor.lightText)
+    static let darkText = Color(UIColor.darkText)
+
     // explicit colours not exposed by SwiftUI
     static let systemGray2 = Color(UIColor.systemGray2)
     static let systemGray3 = Color(UIColor.systemGray3)
